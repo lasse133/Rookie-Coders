113526afc668fb6de06076ea339f14d629e63fe9
Isabel Kaspar <108015110+izzlevershizzle@users.noreply.github.com>
2023-06-26 13:08:34 +0200

    Navigation bar was updated and authentification of user was done

--- Webapp/auth.py
+++ Webapp/auth.py
@@ -2,9 +2,11 @@ from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
+from flask_login import login_user, login_required, logout_user, current_user
 
 
 auth = Blueprint('auth', __name__)
+
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
@@ -15,17 +17,21 @@ def login():
         user = User.query.filter_by(email=email).first()
         if user:
             if check_password_hash(user.password, password):
-                flash('You are logged in now', category='success')
+                flash('You are logged in now!', category='success')
+                login_user(user, remember=True)
+                return redirect(url_for('views.ApplicationPart1'))
             else:
                 flash('Incorrect password. Please try again!', category='error')
         else:
             flash('User account does not exist. Please sign up first.', category='error')
             
-    return render_template("logInPage.html", text="Testing")
+    return render_template("logInPage.html", user=current_user)
 
 @auth.route('/logout')
+@login_required
 def logout():
-    return "<p> logout</p>"
+    logout_user()
+    return redirect(url_for('views.home'))
 
 @auth.route('/sign_up', methods=['GET', 'POST'])
 def sign_up():
@@ -49,10 +55,11 @@ def sign_up():
             new_user = User(name=name, email=email, password=generate_password_hash(password, method='sha256'))
             db.session.add(new_user)
             db.session.commit()
+            login_user(new_user, remember=True)
             flash('You are signed up now!', category='success')
             return redirect(url_for('views.ApplicationPart1'))
         
-    return render_template("signUpPage.html")
+    return render_template("signUpPage.html", user=current_user)
 
 
 
--- Webapp/views.py
+++ Webapp/views.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template
+from flask_login import login_required, current_user
 
 views = Blueprint('views', __name__)
 
@@ -11,28 +12,27 @@ def infoPage():
     return render_template("infoPage.html")
 
 @views.route('ApplicationPart1')
+@login_required
 def ApplicationPart1():
-    return render_template("applicationPartX.html")
+    return render_template("applicationPartX.html", user=current_user)
 
 @views.route('ApplicationPart2')
+@login_required
 def ApplicationPart2():
-    return render_template("applicationPartX.html")
+    return render_template("applicationPartX.html", user=current_user)
 
 @views.route('SubTaskPersonalData')
 def SubTaskPersonalData():
-    return render_template("applicationPartX.html")
+    return render_template("applicationPartX.html", user=current_user)
 
 @views.route('SubTaskAcademicRessources')
 def SubTaskAcademicRessources():
-    return render_template("applicationPartX.html")
+    return render_template("applicationPartX.html", user=current_user)
 
 @views.route('SubTaskFinancialRessources')
 def SubTaskFinancialRessources():
-    return render_template("applicationPartX.html")
+    return render_template("applicationPartX.html", user=current_user)
 
-@views.route('SubTaskForeignInsurance')
-def SubTaskForeignInsurance():
-    return render_template("applicationPartX.html")
 
 
 
