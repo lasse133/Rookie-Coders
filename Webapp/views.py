5bdbcf2c81356343799676bfff49002fd23cbc04
Isabel Kaspar <108015110+izzlevershizzle@users.noreply.github.com>
2023-07-17 14:40:19 +0200

    Subtask Pages and the whole functionalities in terms of routes and html code were added! It looks a little cleaner now as well

--- Webapp/views.py
+++ Webapp/views.py
@@ -19,8 +19,8 @@ def infoPage():
 def ApplicationPart1():
 
     tasks = [
-        {'title': 'First Task', 'description': 'Insert Personal Data', 'page_name' : 'Part 1', 'user_id' : current_user.id},
-        {'title': 'Second Task', 'description': 'Provide Academic Ressources', 'page_name' : 'Part 1', 'user_id' : current_user.id},
+        {'title': 'First Task', 'description': 'Insert Personal Data (See Documents for Details)', 'page_name' : 'Part 1', 'user_id' : current_user.id},
+        {'title': 'Second Task', 'description': 'Provide Academic Ressources (See Documents for Details)', 'page_name' : 'Part 1', 'user_id' : current_user.id},
         {'title': 'Third Task', 'description': 'Write Letter of Motivation', 'page_name' : 'Part 1', 'user_id' : current_user.id},
         {'title': 'Fourth Task', 'description': 'Course Selection', 'page_name' : 'Part 1', 'user_id' : current_user.id},
         {'title': 'Fifth Task', 'description': 'Send Off First Part of the Application', 'page_name' : 'Part 1', 'user_id' : current_user.id}
@@ -109,6 +109,41 @@ def addPart2():
         flash('Task added', category='success')
     return redirect(url_for('views.ApplicationPart2'))
 
+@views.route('/addTaskPersonalData', methods=['POST'])
+def addPersonalData():
+    if request.method == 'POST':
+        title = request.form.get('title')
+        description = request.form.get('description')
+        new_task = Task(title=title, description=description, page_name='Personal Data', user_id=current_user.id)
+        db.session.add(new_task)
+        db.session.commit()
+        flash('Task added', category='success')
+    return redirect(url_for('views.SubTaskPersonalData'))
+
+
+@views.route('/addTaskAcademicRessources', methods=['POST'])
+def addAcademicRessources():
+    if request.method == 'POST':
+        title = request.form.get('title')
+        description = request.form.get('description')
+        new_task = Task(title=title, description=description, page_name='Academic Ressources', user_id=current_user.id)
+        db.session.add(new_task)
+        db.session.commit()
+        flash('Task added', category='success')
+    return redirect(url_for('views.SubTaskAcademicRessources'))
+
+
+@views.route('/addTaskFinancialRessources', methods=['POST'])
+def addFinancialRessources():
+    if request.method == 'POST':
+        title = request.form.get('title')
+        description = request.form.get('description')
+        new_task = Task(title=title, description=description, page_name='Financial Ressources', user_id=current_user.id)
+        db.session.add(new_task)
+        db.session.commit()
+        flash('Task added', category='success')
+    return redirect(url_for('views.SubTaskFinancialRessources'))
+
 @views.route('/completeTaskPart1/<task_id>')
 def complete_taskPart1(task_id):
     task = Task.query.filter_by(id=task_id, page_name='Part 1').first()
@@ -124,6 +159,33 @@ def complete_taskPart2(task_id):
     db.session.commit()
     flash('Task done', category='success')
     return redirect(url_for('views.ApplicationPart2'))
+
+
+@views.route('/completeTaskPersonalData/<task_id>')
+def complete_taskPersonalData(task_id):
+    task = Task.query.filter_by(id=task_id, page_name='Personal Data').first()
+    task.completed = not task.completed
+    db.session.commit()
+    # flash('Task done', category='success')
+    return redirect(url_for('views.SubTaskPersonalData'))
+
+
+@views.route('/completeTaskAcademicRessources/<task_id>')
+def complete_taskAcademicRessources(task_id):
+    task = Task.query.filter_by(id=task_id, page_name='Academic Ressources').first()
+    task.completed = not task.completed
+    db.session.commit()
+    # flash('Task done', category='success')
+    return redirect(url_for('views.SubTaskAcademicRessources'))
+
+
+@views.route('/completeTaskFinancialRessources/<task_id>')
+def complete_taskFinancialRessources(task_id):
+    task = Task.query.filter_by(id=task_id, page_name='Financial Ressources').first()
+    task.completed = not task.completed
+    db.session.commit()
+    # flash('Task done', category='success')
+    return redirect(url_for('views.SubTaskFinancialRessources'))
 
 @views.route('/deleteTaskPart1/<task_id>')
 def delete_taskPart1(task_id):
@@ -141,25 +203,62 @@ def delete_taskPart2(task_id):
     flash('Task deleted', category='success')
     return redirect(url_for('views.ApplicationPart2'))
 
+@views.route('/deleteTaskPersonalData/<task_id>')
+def delete_taskPersonalData(task_id):
+    task = Task.query.filter_by(id=task_id, page_name='Personal Data').first()
+    db.session.delete(task)
+    db.session.commit()
+    flash('Task deleted', category='success')
+    return redirect(url_for('views.SubTaskPersonalData'))
+
+@views.route('/deleteTaskAcademicRessources/<task_id>')
+def delete_taskAcademicRessources(task_id):
+    task = Task.query.filter_by(id=task_id, page_name='Academic Ressources').first()
+    db.session.delete(task)
+    db.session.commit()
+    flash('Task deleted', category='success')
+    return redirect(url_for('views.SubTaskAcademicRessources'))
+
+@views.route('/deleteTaskFinancialRessources/<task_id>')
+def delete_taskFinancialRessources(task_id):
+    task = Task.query.filter_by(id=task_id, page_name='Financial Ressources').first()
+    db.session.delete(task)
+    db.session.commit()
+    flash('Task deleted', category='success')
+    return redirect(url_for('views.SubTaskFinancialRessources'))
+
 @views.route('/save-datePart1', methods=['POST'])
 def save_datePart1():
     if request.method == 'POST':
-        date = request.form.get('date')
-        new_date = Date(date=date, page_name='Part 1', user_id=current_user.id)
-        db.session.add(new_date)
-        db.session.commit()
-        flash('Date saved', category='success')
-        return redirect(url_for('views.ApplicationPart1'))
+        existing_date = Date.query.filter_by(page_name='Part 1', user_id=current_user.id).first()
+
+        if existing_date:
+            flash('You have already added a date. Please delete your existing date before adding a new one.', category='error')
+        else:
+            date = request.form.get('date')
+            new_date = Date(date=date, page_name='Part 1', user_id=current_user.id)
+            db.session.add(new_date)
+            db.session.commit()
+            flash('Date saved', category='success')
+
+    return redirect(url_for('views.ApplicationPart1'))
+
     
 @views.route('/save-datePart2', methods=['POST'])
 def save_datePart2():
     if request.method == 'POST':
-        date = request.form.get('date')
-        new_date = Date(date=date, page_name='Part 2', user_id=current_user.id)
-        db.session.add(new_date)
-        db.session.commit()
-        flash('Date saved', category='success')
-        return redirect(url_for('views.ApplicationPart2'))
+        existing_date = Date.query.filter_by(page_name='Part 2', user_id=current_user.id).first()
+
+        if existing_date:
+            flash('You have already added a date. Please delete your existing date before adding a new one.', category='error')
+        else:
+            date = request.form.get('date')
+            new_date = Date(date=date, page_name='Part 2', user_id=current_user.id)
+            db.session.add(new_date)
+            db.session.commit()
+            flash('Date saved', category='success')
+
+    return redirect(url_for('views.ApplicationPart2'))
 
 
 @views.route('/displayDatePart1/<date_id>')
@@ -195,7 +294,7 @@ def delete_datePart2(date_id):
 def ApplicationPart2():
     
     tasks = [
-        {'title': 'Sixth Task', 'description': 'Insert Personal Data Part Two', 'page_name' : 'Part 2', 'user_id' : current_user.id},
+        {'title': 'Sixth Task', 'description': 'Insert Personal Data Part Two (See Documents for Details)', 'page_name' : 'Part 2', 'user_id' : current_user.id},
         {'title': 'Seventh Task', 'description': 'Provide Fincancial Sources', 'page_name' : 'Part 2', 'user_id' : current_user.id},
         {'title': 'Eighth Task', 'description': 'Send off Second Part of the Application', 'page_name' : 'Part 2', 'user_id' : current_user.id},
         {'title': 'Ninth Task', 'description': 'Arrange Appointment with Consulate', 'page_name' : 'Part 2', 'user_id' : current_user.id},
@@ -225,14 +324,84 @@ def ApplicationPart2():
 
 @views.route('SubTaskPersonalData', methods=['GET', 'POST'])
 def SubTaskPersonalData():
+    tasks = [
+        {'title': 'First Document', 'description': 'Copy of Passport', 'page_name' : 'Personal Data', 'user_id' : current_user.id},
+        {'title': 'Second Document', 'description': 'Covid-19 Vaccination Certificate', 'page_name' : 'Personal Data', 'user_id' : current_user.id},
+        {'title': 'Third Document', 'description': 'Vaccination Certificate', 'page_name' : 'Personal Data', 'user_id' : current_user.id},
+        ]
+
+    for task in tasks:
+            title = task['title']
+            description = task['description']
+            page_name = task['page_name']
+            user_id = task['user_id']
+
+             # Check if the task already exists in the database
+            existing_task = Task.query.filter_by(title=title, description=description, page_name=page_name, user_id = user_id).first()
+
+            if existing_task:
+                print("Task already exists in DB.")
+                # Task already exists
+
+            else:
+                new_task = Task(title=title, description=description, page_name=page_name, user_id = user_id)
+                db.session.add(new_task)
+
+    db.session.commit()
     return render_template("subTaskPersonal.html", user=current_user)
 
 @views.route('SubTaskAcademicRessources', methods=['GET', 'POST'])
 def SubTaskAcademicRessources():
+    tasks = [
+        {'title': 'First Document', 'description': 'Transcripts', 'page_name' : 'Academic Ressources', 'user_id' : current_user.id},
+        {'title': 'Second Document', 'description': 'Perfomance Overview', 'page_name' : 'Academic Ressources', 'user_id' : current_user.id},
+        ]
+
+    for task in tasks:
+            title = task['title']
+            description = task['description']
+            page_name = task['page_name']
+            user_id = task['user_id']
+
+             # Check if the task already exists in the database
+            existing_task = Task.query.filter_by(title=title, description=description, page_name=page_name, user_id = user_id).first()
+
+            if existing_task:
+                print("Task already exists in DB.")
+                # Task already exists
+
+            else:
+                new_task = Task(title=title, description=description, page_name=page_name, user_id = user_id)
+                db.session.add(new_task)
+
+    db.session.commit()
     return render_template("subTaskAcademic.html", user=current_user)
 
 @views.route('SubTaskFinancialRessources', methods=['GET', 'POST'])
 def SubTaskFinancialRessources():
+    tasks = [
+        {'title': 'First Document', 'description': 'Proof of Funding', 'page_name' : 'Financial Ressources', 'user_id' : current_user.id},
+        {'title': 'Second Document', 'description': 'Proof of Scholarship', 'page_name' : 'Financial Ressources', 'user_id' : current_user.id},
+        ]
+
+    for task in tasks:
+            title = task['title']
+            description = task['description']
+            page_name = task['page_name']
+            user_id = task['user_id']
+
+             # Check if the task already exists in the database
+            existing_task = Task.query.filter_by(title=title, description=description, page_name=page_name, user_id = user_id).first()
+
+            if existing_task:
+                print("Task already exists in DB.")
+                # Task already exists
+
+            else:
+                new_task = Task(title=title, description=description, page_name=page_name, user_id = user_id)
+                db.session.add(new_task)
+
+    db.session.commit()
     return render_template("subTaskFinancial.html", user=current_user)
 
 
